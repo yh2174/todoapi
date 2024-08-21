f6087d41a9de056198a4f7e0d23e33168c3aa96d
yh2174 <ytbrru1324@naver.com>
2024-08-21 19:52:10 +0900

    Update Maincontroller.java

--- demo/src/main/java/com/example/demo/controller/Maincontroller.java
+++ demo/src/main/java/com/example/demo/controller/Maincontroller.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class MainController {
+public class Maincontroller {
     @GetMapping("/")
     public String welcome() {
         return "Hello World!";
